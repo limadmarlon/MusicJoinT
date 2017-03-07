a25d8f770e1c6d30704ed762ff6ff714d844cc38
limadmarlon <limadmarlon@gmail.com>
2017-03-06 22:19:00 -0300

    First Commit

---
+++ MusicJoinT/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MusicJoinT
+//
+//  Created by Student on 3/6/17.
+//  Copyright © 2017 Leonardo Tolêdo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
