d3ccb0c9973c7ce4c9e64f6f58fdcd7559baf8de
limadmarlon <limadmarlon@gmail.com>
2017-03-06 23:52:04 -0300

    update

---
+++ MusicJoinT/HomeTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  HomeTableViewCell.swift
+//  MusicJoinT
+//
+//  Created by Marlon Lima on 3/6/17.
+//  Copyright © 2017 Leonardo Tolêdo. All rights reserved.
+//
+
+import UIKit
+
+class HomeTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
